bacacc84338182daf72406cca8f01f880d8f6c3a
ProdenceM <dp.marubelela@gmail.com>
2023-02-20 13:10:59 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+
+}
