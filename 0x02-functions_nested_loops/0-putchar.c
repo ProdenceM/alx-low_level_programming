744630b9c32983102c0beec0f1e34cde80fbb565
ProdenceM <dp.marubelela@gmail.com>
2023-02-22 07:17:04 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	-putchar('u');
-	-putchar('t');
-	-putchar('c');
-	-putchar('h');
-	-putchar('a');
-	-putchar('r');
-	-putchar('\n');
-	return(0);
+	_putchar ('_');
+	_putchar ('p');
+	-putchar ('u');
+	-putchar ('t');
+	-putchar ('c');
+	-putchar ('h');
+	-putchar ('a');
+	-putchar ('r');
+	-putchar ('\n');
+	return (0);
+
 }
