825d9d45ccf9d36351b2f106f46dd8bcc33faa9f
ProdenceM <dp.marubelela@gmail.com>
2023-03-07 03:55:00 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- *main - entry point
- *Return: always 0 (sucess)
- */
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
